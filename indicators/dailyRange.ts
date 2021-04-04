4558c9456dbf81e47ae2e5999b48cfd154e09e0d
paidtofade <paidtofade@users.noreply.github.com>
2021-04-04 09:47:15 -0500

    Change ATR box to daily range, add some other options

---
+++ indicators/dailyRange.ts
@@ -0,0 +1,51 @@
+# Created by @paidtofade in 2021
+#
+# If you find this indicator helpful, give me some love on twitter @paidtofade
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+declare upper;
+
+input rangeType = { default ATR, ADR, StdDev };
+input averageType = AverageType.SIMPLE;
+input length = 20;
+input multiplier = 1.0;
+input baseLineType = { default TodayOpen, YesterdayClose };
+input showBaseLine = yes;
+
+def dayOpen = open(period = AggregationPeriod.DAY);
+def dayHigh = high(period = AggregationPeriod.DAY);
+def dayLow = low(period = AggregationPeriod.DAY);
+def dayClose = close(period = AggregationPeriod.DAY);
+
+def dailyRange;
+switch (rangeType) {
+    case ATR:
+      dailyRange = MovingAverage(
+          averageType, TrueRange(dayHigh, dayClose, dayLow), length);
+    case StdDev:
+      dailyRange = StDev(dayClose, length) * 2.0;
+    case ADR:
+      dailyRange = MovingAverage(averageType, dayHigh - dayLow, length);
+}
+
+def baseLineVal = if baseLineType == baseLineType.TodayOpen then dayOpen else dayClose[1];
+
+plot rangeHigh = baseLineVal + ((dailyRange[1] / 2) * multiplier);
+plot rangeLow = baseLineVal - ((dailyRange[1] / 2) * multiplier);
+plot baseLine = if showBaseLine then baseLineVal else Double.NaN;
+
+rangeHigh.SetDefaultColor(Color.GREEN);
+rangeLow.SetDefaultColor(Color.GREEN);
+baseLine.SetDefaultColor(Color.CYAN);
